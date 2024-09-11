ae4f64702efbe0bb2a376efa528e50ad785d8df4
[AI10] 임지영 <dlawldud021@naver.com>
2024-09-11 14:26:37 +0900

    week02

--- Week_02/Week_02.c
+++ Week_02/Week_02.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    printf("Hello, Sookmyung Electronics!\n");
+int main(void) {
+    int a, b; //variables
 
+    a = 10;
+    b = 20;
+
+    printf("Hello, Sookmyung Electronics! \n");
+    printf("%d + %d = %d \n", a, b, a+b);
+    
     return 0;
 }
