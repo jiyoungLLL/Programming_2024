b6210e97cb6fb7171fef8af32943dd6da3bdd40b
[AI10] 임지영 <dlawldud021@naver.com>
2024-11-20 15:52:25 +0900

    12주차 실습

--- Week12/practice4.c
+++ Week12/practice4.c
@@ -6,15 +6,16 @@ int main(void) {
     int i;
     int grade[5];
     int sum = 0;
+    int *p;
 
     for(i=0;i<5;i++) {
         printf("Input value - grade[%i] = ", i);
         scanf("%d", &grade[i]);
     }
 
-    for(i=0; i<5; i++) {
-        printf("grade[%i] = %i \n", i, grade[i]);
-        sum += grade[i];
+    for(p=grade; p<grade + 5; p++) {
+        printf("grade[%i] = %i \n", p-grade, *p);
+        sum += *p;
     }
 
     printf("average: %i \n", sum/5);
